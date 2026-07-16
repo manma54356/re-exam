7969f5f636a7b67722838cbe903c42b1ae7642ba
manma54356 <239759736+manma54356@users.noreply.github.com>
2026-07-16 15:16:31 +0800

    修復 106 年全科目測驗題（180 題選項重建）＋補回 106 國文作文題

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'reexam-v1';
+const CACHE = 'reexam-v2';
 const ASSETS = ['./', 'index.html', 'manifest.webmanifest', 'icon-180.png', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', (e) => {
