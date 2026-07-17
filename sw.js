7a856e0105e4693d2107cef8d97d658fc67674f8
manma54356 <239759736+manma54356@users.noreply.github.com>
2026-07-17 10:30:22 +0800

    重建 91 憲法 80 題＋92第1次全 4 科 160 題；全庫 Unicode 正規化

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'reexam-v3';
+const CACHE = 'reexam-v4';
 const ASSETS = ['./', 'index.html', 'manifest.webmanifest', 'icon-180.png', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', (e) => {
